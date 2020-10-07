1a695d5accbb747aed0352338d105ffcfc9c57be
huongnguyen307 <huongnguyendo307@gmail.com>
2020-10-07 16:41:08 +0700

    first commit

--- controller.js
+++ controller.js
@@ -1,62 +1,61 @@
 const fs = require("fs");
 
 const loadData = () => {
-    let todoList = fs.readFileSync("data.json").toString();
-    todoList = JSON.parse(todoList);
+  let todoList = fs.readFileSync("data.json").toString();
+  todoList = JSON.parse(todoList);
 
-    return todoList;
-}
+  return todoList;
+};
 
 const saveData = (todoList) => {
-    // save todoList into json file
-    const todoJSON = JSON.stringify(todoList);
-    // save into file
-    fs.writeFileSync("data.json", todoJSON);
-}
+  // save todoList into json file
+  const todoJSON = JSON.stringify(todoList);
+  // save into file
+  fs.writeFileSync("data.json", todoJSON);
+};
 
 const createTodo = (todo, status) => {
-    const todoList = loadData();
+  const todoList = loadData();
 
-    if (todoList.length == 0) {
-        id = 0;
-    } else {
-        id = todoList[todoList.length - 1].id + 1;
-    }
+  if (todoList.length == 0) {
+    id = 0;
+  } else {
+    id = todoList[todoList.length - 1].id + 1;
+  }
 
-    todoList.push({id: id, todo: todo, status: status});
-    
-    saveData(todoList);
-    return data;
-}
+  todoList.push({ id: id, todo: todo, status: status });
+
+  saveData(todoList);
+  return data;
+};
 
 function deleteTodo(id) {
-    const data = loadData();
+  const data = loadData();
 
-    const index = data.findIndex((item) => item.id === id);
-    console.log("index ", index);
+  const index = data.findIndex((item) => item.id === id);
+  console.log("index ", index);
 
-    data.splice(index,1);
+  data.splice(index, 1);
 
-    saveData(data);
-    return data;
+  saveData(data);
+  return data;
 }
 
 function toggleTodo(id) {
-    const data = loadData();
-    
-    const index = data.findIndex((item) => item.id === id);
-    
-    data[index].status = !data[index].status;
+  const data = loadData();
 
+  const index = data.findIndex((item) => item.id === id);
 
-    saveData(data);
-    return data;
+  data[index].status = !data[index].status;
+
+  saveData(data);
+  return data;
 }
 
-module.exports={ 
-    loadData: loadData,
-    saveData: saveData,
-    createTodo: createTodo,
-    deleteTodo: deleteTodo,
-    toggleTodo: toggleTodo,
-}
+module.exports = {
+  loadData: loadData,
+  saveData: saveData,
+  createTodo: createTodo,
+  deleteTodo: deleteTodo,
+  toggleTodo: toggleTodo,
+};
